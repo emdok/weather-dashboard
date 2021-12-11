ed3c942d9cdff493a3c956177802df5c1d997c58
Emily Dokken <dokkenemily@gmail.com>
2021-12-11 14:50:05 -0800

    get forecast by city using lat and lon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,38 @@
 var APIKey = '53fc86921cad0b3c977522d6827674ab';
-// var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + "&appid=" + APIKey;
+var city = "Portland";
 
 
-fetch(queryURL).then(function() {
-    console.log(queryURL);
-})
 
+var getCityCoord = function(city) {
+    var cityCoordQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
+
+    fetch(cityCoordQueryURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                getForecastByCity(data);
+            });
+        } else {
+            alert("Error: Current weather for City not found");
+        }
+    });
+}
+
+var getForecastByCity = function(data) {
+    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=" + APIKey;
+
+    fetch(forecastQueryURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        } else {
+            alert("Error: Forecast for City not found");
+        }
+});
+}
+
+getCityCoord(city);
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
