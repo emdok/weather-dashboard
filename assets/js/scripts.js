9f1647678b91a6b37e469ebf183d005c05a4fd5a
Emily Dokken <dokkenemily@gmail.com>
2021-12-11 14:59:25 -0800

    add event handler for button submit when a user inputs a city

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,9 @@
 var APIKey = '53fc86921cad0b3c977522d6827674ab';
-var city = "Portland";
+var cityInputEl = document.querySelector("#city-search");
+var cityFormEl = document.querySelector("#city-form");
 
 
-
+// Get city and locate lat and lon values
 var getCityCoord = function(city) {
     var cityCoordQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
 
@@ -17,6 +18,7 @@ var getCityCoord = function(city) {
     });
 }
 
+// Update city to lat and lon value, return forecast for city
 var getForecastByCity = function(data) {
     var forecastQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&appid=" + APIKey;
 
@@ -31,8 +33,19 @@ var getForecastByCity = function(data) {
 });
 }
 
-getCityCoord(city);
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var city = cityInputEl.value.trim();
+
+    if (city) {
+        getCityCoord(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a city");
+    }
+};
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
+
 
