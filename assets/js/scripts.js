57cd234ddd04e310999d94419750fcd0e2a293a0
Emily Dokken <dokkenemily@gmail.com>
2021-12-11 12:37:24 -0800

    update style sheet, add bootstrap, add search field and button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
-var OpenWeatherMapAPIKey = '53fc86921cad0b3c977522d6827674ab';
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var APIKey = '53fc86921cad0b3c977522d6827674ab';
+// var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + "&appid=" + APIKey;
 
 
 fetch(queryURL).then(function() {
