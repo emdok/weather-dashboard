d986a8404e786e6b4f1c4bedb6df5c4b6f47f11f
Emily Dokken <dokkenemily@gmail.com>
2021-12-11 12:10:43 -0800

    add initial files

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+var OpenWeatherMapAPIKey = '53fc86921cad0b3c977522d6827674ab';
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+fetch(queryURL).then(function() {
+    console.log(queryURL);
+})
+
+
